2a1d57eaebedbe649f389e3645588358877aec62
flutterajwa <flutterajwa@gmail.com>
2026-09-06 10:25:12 +0530

    udated for livee

---
+++ start-website.bat
@@ -0,0 +1,6 @@
+@echo off
+title Dua Mehavish Photography Storybook
+echo Starting Vite Web Server...
+start "" http://localhost:3000/
+npm run dev
+pause
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/duamehavish/',
   plugins: [react()],
   server: {
     port: 3000,
